168c84cc6f9ed784413fff08afbbcb92fc922f02
Velyn-N <nils01.gaming@gmail.com>
2026-06-07 17:27:42 +0200

    Separate the Module's Documentation Sections (similar to PaperMC)

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,18 @@ export default defineConfig({
 			favicon: '/favicon.png',
 			logo: { src: './src/assets/RPCauldron.png' },
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/roleplay-cauldron' }],
+			components: {
+				Sidebar: './src/components/ModuleIsolatingSidebar.astro',
+			},
 			sidebar: [
 				{
 					label: 'Brotkrumen',
+					collapsed: true,
 					items: [{ autogenerate: { directory: 'brotkrumen' } }],
 				},
 				{
 					label: 'Spellbook',
+					collapsed: true,
 					items: [{ autogenerate: { directory: 'spellbook' } }],
 				},
 			],
