505ab0f20bce75109e100078d4c5929593b19fcf
Velyn-N <nils01.gaming@gmail.com>
2026-06-15 12:24:06 +0200

    Custom Theme Select and Docs Landingpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightSidebarTopics from 'starlight-sidebar-topics'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'RolePlay Cauldron',
-			favicon: '/favicon.png',
-			logo: { src: './src/assets/RPCauldron.png' },
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/roleplay-cauldron' }],
-			customCss: ['./src/assets/global.css'],
-			plugins: [
-				starlightSidebarTopics([
-					{
-						label: 'Brotkrumen',
-						link: '/brotkrumen/',
-						icon: 'random',
-						items: [{ autogenerate: { directory: 'brotkrumen' } }],
-					},
-					{
-						label: 'Spellbook',
-						link: '/spellbook/',
-						icon: 'open-book',
-						items: [{ autogenerate: { directory: 'spellbook' } }],
-					},
-				]),
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'RolePlay Cauldron',
+            favicon: '/favicon.png',
+            logo: {src: './src/assets/RPCauldron.png'},
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/roleplay-cauldron'}],
+            customCss: ['./src/assets/global.css'],
+            components: {
+                ThemeSelect: './src/components/ThemeToggle.astro',
+            },
+            plugins: [
+                starlightSidebarTopics([
+                        {
+                            label: 'Brotkrumen',
+                            link: '/brotkrumen/',
+                            icon: 'random',
+                            items: [{autogenerate: {directory: 'brotkrumen'}}],
+                        },
+                        {
+                            label: 'Spellbook',
+                            link: '/spellbook/',
+                            icon: 'open-book',
+                            items: [{autogenerate: {directory: 'spellbook'}}],
+                        },
+                    ],
+                    {
+                        exclude: ['/docs'],
+                    },
+                ),
+            ],
+        }),
+    ],
 });
